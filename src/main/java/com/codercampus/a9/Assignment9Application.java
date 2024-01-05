d355caf5b384b1c06be86f64e633e7a1ee7bcf68
jeffpodmayer <148505528+jeffpodmayer@users.noreply.github.com>
2024-01-05 12:59:08 -0800

    Setup FileService class

--- src/main/java/com/codercampus/a9/Assignment9Application.java
+++ src/main/java/com/codercampus/a9/Assignment9Application.java
@@ -1,13 +1,19 @@
 package com.codercampus.a9;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.coderscampus.a9.service.FileService;
 
 @SpringBootApplication
 public class Assignment9Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(Assignment9Application.class, args);
+		
+		FileService.readFile();
 	}
 
 }
--- src/main/java/com/coderscampus/a9/service/FileService.java
+++ src/main/java/com/coderscampus/a9/service/FileService.java
@@ -1,5 +1,34 @@
 package com.coderscampus.a9.service;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+
 public class FileService {
 
+	public static void readFile() throws IOException {
+		Reader in = new FileReader("recipes.txt");
+
+		Iterable<CSVRecord> records = CSVFormat.DEFAULT.withIgnoreSurroundingSpaces().parse(in);
+		
+		for(CSVRecord record:records){
+			Integer cookingMinutes = Integer.parseInt(record.get(0));
+			Boolean dairyFree = Boolean.parseBoolean(record.get(1));
+			Boolean glutenFree = Boolean.parseBoolean(record.get(2));
+			String instructions = record.get(3);
+			Double prepMinutes = Double.parseDouble(record.get(4));
+			Double pricePerServing = Double.parseDouble(record.get(5));
+			Integer readyInMinutes = Integer.parseInt(record.get(6));
+			Integer servings = Integer.parseInt(record.get(7));
+			Double spoonacularScore = Double.parseDouble(record.get(8));
+			String title = record.get(9);
+			Boolean vegan = Boolean.parseBoolean(record.get(10));
+			Boolean vegetarian = Boolean.parseBoolean(record.get(11));
+			
+			
+		}
+	}
 }
