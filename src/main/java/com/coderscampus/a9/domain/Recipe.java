e35514c47d4161cf3f2dc976c5a7b42aabbffc98
jeffpodmayer <148505528+jeffpodmayer@users.noreply.github.com>
2024-01-08 13:28:38 -0800

    updated POJO and FileService Class

--- src/main/java/com/coderscampus/a9/domain/Recipe.java
+++ src/main/java/com/coderscampus/a9/domain/Recipe.java
@@ -5,7 +5,7 @@ public class Recipe {
 	private Boolean dairyFree;
 	private Boolean glutenFree;
 	private String instructions;
-	private Double preparationMinutes;
+	private Double prepMinutes;
 	private Double pricePerServing;
 	private Integer readyInMinutes;
 	private Integer servings;
@@ -13,6 +13,23 @@ public class Recipe {
 	private String title;
 	private Boolean vegan;
 	private Boolean vegetarian;
+	
+	public Recipe (Integer cookingMinutes, Boolean dairyFree, Boolean glutenFree, String instructions, Double prepMinutes, 
+			Double pricePerServing, Integer readyInMinutes, Integer servings, Double spoonacularScore, String title, Boolean vegan, 
+			Boolean vegetarian) {
+		this.cookingMinutes = cookingMinutes;
+		this.dairyFree = dairyFree;
+		this.glutenFree = glutenFree;
+		this.instructions = instructions;
+		this.prepMinutes = prepMinutes;
+		this.pricePerServing = pricePerServing;
+		this.readyInMinutes = readyInMinutes;
+		this.servings = servings;
+		this.spoonacularScore = spoonacularScore;
+		this.title = title;
+		this.vegan = vegan;
+		this.vegetarian = vegetarian;	
+	}
 	public Integer getCookingMinutes() {
 		return cookingMinutes;
 	}
@@ -38,10 +55,10 @@ public class Recipe {
 		this.instructions = instructions;
 	}
 	public Double getPreparationMinutes() {
-		return preparationMinutes;
+		return prepMinutes;
 	}
 	public void setPreparationMinutes(Double preparationMinutes) {
-		this.preparationMinutes = preparationMinutes;
+		this.prepMinutes = preparationMinutes;
 	}
 	public Double getPricePerServing() {
 		return pricePerServing;
--- src/main/java/com/coderscampus/a9/service/FileService.java
+++ src/main/java/com/coderscampus/a9/service/FileService.java
@@ -3,6 +3,7 @@ package com.coderscampus.a9.service;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.StreamSupport;
 
@@ -10,31 +11,36 @@ import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 import org.springframework.stereotype.Service;
 
+import com.coderscampus.a9.domain.Recipe;
+
 @Service
 public class FileService {
-
-	public List<CSVRecord> readFile() throws IOException {
-		Reader in = new FileReader("recipes.txt");
-
-		Iterable<CSVRecord> records = CSVFormat.DEFAULT.withIgnoreSurroundingSpaces().parse(in);
-
-		for (CSVRecord record : records) {
-			Integer cookingMinutes = Integer.parseInt(record.get(0));
-			Boolean dairyFree = Boolean.parseBoolean(record.get(1));
-			Boolean glutenFree = Boolean.parseBoolean(record.get(2));
-			String instructions = record.get(3);
-			Double prepMinutes = Double.parseDouble(record.get(4));
-			Double pricePerServing = Double.parseDouble(record.get(5));
-			Integer readyInMinutes = Integer.parseInt(record.get(6));
-			Integer servings = Integer.parseInt(record.get(7));
-			Double spoonacularScore = Double.parseDouble(record.get(8));
-			String title = record.get(9);
-			Boolean vegan = Boolean.parseBoolean(record.get(10));
-			Boolean vegetarian = Boolean.parseBoolean(record.get(11));
+	List<Recipe> recipeList = new ArrayList<>();
+	
+	public List<Recipe> readFile() throws IOException {
+		Reader readRecipes = new FileReader("recipes.txt");
+		Iterable<CSVRecord> recipes = CSVFormat.DEFAULT.withSkipHeaderRecord().withIgnoreSurroundingSpaces().parse(readRecipes);
+		
+		for (CSVRecord recipe : recipes) {
+			Recipe newRecipe = new Recipe(
+					Integer.parseInt(recipe.get(0)),
+					Boolean.parseBoolean(recipe.get(1)),
+					Boolean.parseBoolean(recipe.get(2)),
+					recipe.get(3),
+					Double.parseDouble(recipe.get(4)),
+					Double.parseDouble(recipe.get(5)),
+					Integer.parseInt(recipe.get(6)),
+					Integer.parseInt(recipe.get(7)),
+					Double.parseDouble(recipe.get(8)),
+					recipe.get(9),
+					Boolean.parseBoolean(recipe.get(10)),
+					Boolean.parseBoolean(recipe.get(11))
+			);
+			
+			recipeList.add(newRecipe);
 		}
 		
-		List<CSVRecord> collection = StreamSupport.stream(records.spliterator(), false)
-													.toList();	
-		return 	collection;	
+		
+		return recipeList;	
 	}
 }
